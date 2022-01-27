d2969f27c3a043fa1f1a7d5f7bede929d5778abd
youngkwon02 <youngkwon02@naver.com>
2022-01-27 19:53:34 +0900

    feat: RateDiscountPolicy

---
+++ core/src/main/java/com/helloJava/core/discount/RateDiscountPolicy.java
@@ -0,0 +1,19 @@
+package com.helloJava.core.discount;
+
+import com.helloJava.core.member.Grade;
+import com.helloJava.core.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy {
+
+    private int discountPercent = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if(member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
+    }
+
+}
