90c424c43cfb9c0817127e6c61b756ab20d85c62
youngkwon02 <youngkwon02@naver.com>
2022-02-04 02:23:49 +0900

    feat: component scan filter

---
+++ src/test/java/com/helloJava/core/scan/filter/BeanA.java
@@ -0,0 +1,5 @@
+package com.helloJava.core.scan.filter;
+
+@MyIncludeComponent
+public class BeanA {
+}
---
+++ src/test/java/com/helloJava/core/scan/filter/BeanB.java
@@ -0,0 +1,5 @@
+package com.helloJava.core.scan.filter;
+
+@MyExcludeComponent
+public class BeanB {
+}
